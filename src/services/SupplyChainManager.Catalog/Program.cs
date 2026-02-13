6dd2b5e626432f3e644f9a74075dffa6e2263e92
aos <aos_5766@hotmail.com>
2026-02-13 14:39:23 +0300

    Initial Commit

---
+++ src/services/SupplyChainManager.Catalog/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi("v1", options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info.Title = "Supply Chain Catalog Service API";
+        document.Info.Version = "v1.0.0";
+        document.Info.Description = "Gateway API for Supply Chain Manager FE";
+        return Task.CompletedTask;
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapGet("/", () => Results.Redirect("/openapi/v1.json"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
